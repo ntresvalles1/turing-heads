750b4ddf2935401fe710e75bf36e09db990d46f3
nicole tresvalles <76967102+ntresvalles1@users.noreply.github.com>
2023-08-25 16:36:10 -0700

    Add files via upload

---
+++ wordpress-playground/packages/playground/website/src/components/browser-chrome/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import css from './style.module.css';
+import AddressBar from '../address-bar';
+import classNames from 'classnames';
+
+interface BrowserChromeProps {
+	children?: React.ReactNode;
+	toolbarButtons?: Array<React.ReactElement | false | null>;
+	url?: string;
+	showAddressBar?: boolean;
+	onUrlChange?: (url: string) => void;
+}
+
+export default function BrowserChrome({
+	children,
+	url,
+	onUrlChange,
+	showAddressBar = false,
+	toolbarButtons,
+}: BrowserChromeProps) {
+	const addressBarClass = classNames(css.addressBarSlot, {
+		[css.isHidden]: !showAddressBar,
+	});
+	return (
+		<div className={css.wrapper}>
+			<div className={css.window}>
+				<div className={css.toolbar}>
+					<WindowControls />
+
+					<div className={addressBarClass}>
+						<AddressBar url={url} onUpdate={onUrlChange} />
+					</div>
+
+					<div className={css.toolbarButtons}>{toolbarButtons}</div>
+				</div>
+				<div className={css.content}>{children}</div>
+			</div>
+		</div>
+	);
+}
+
+function WindowControls() {
+	return (
+		<div className={css.windowControls}>
+			<div className={`${css.windowControl} ${css.isNeutral}`}></div>
+			<div className={`${css.windowControl} ${css.isNeutral}`}></div>
+			<div className={`${css.windowControl} ${css.isNeutral}`}></div>
+		</div>
+	);
+}
